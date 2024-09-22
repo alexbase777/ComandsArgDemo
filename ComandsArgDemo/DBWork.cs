b5c6c2683380d62bcb224820ff2869eb252c64c1
Алексей <tm23515417@gmail.com>
2024-09-22 16:05:43 +0500

    Add project files.

---
+++ ComandsArgDemo/DBWork.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComandsArgDemo
+{
+    internal class DBWork
+    {
+        static private string dbname = "Hobbits.db";
+        static private string path = $"Data Source={dbname}";
+
+        static public bool IfDBExists() // Метод проверки существования БД
+        {            
+            if (File.Exists(dbname)) return true;
+            return false;
+        }
+        static public void MakeBD() // Метод создания БД
+        {             
+            MakeQuery("CREATE TABLE IF NOT EXISTS " +
+                " Names (id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                " number_arg VARCHAR, " +
+                " name VARCHAR, " +
+                "date_time TEXT);"); // Подключаемся к БД и выполняем запрос
+        }
+        static public void InitDB(string arg, string name) // Метод заполнения БД
+        {            
+            MakeQuery("INSERT INTO Names(number_arg, name, date_time) " +
+                "VALUES " +
+                $"('{arg}', '{name}', '{DateTime.Now.ToString("yyyyMMdd-HH:mm")}');"); // Подключаемся к БД и выполняем запрос
+        }
+        static public void DropTable() // Метод очистки содержимого таблицы
+        {            
+            MakeQuery("DROP TABLE Names;"); // Подключаемся к БД и выполняем запрос
+        }
+        static public string GetData(int id, string value) // Метод получения данных из БД
+        {               
+            string result = string.Empty;
+            using (SQLiteConnection conn = new SQLiteConnection(path)) // Ресурс для подключения к БД
+            {
+                SQLiteCommand cmd = conn.CreateCommand();  // Создаём в ресурсе команду на выполнение запроса
+                cmd.CommandText = $"SELECT {value} FROM Names WHERE id = {id};"; // Формируем конкретную команду
+                conn.Open(); // Открываем соединение
+                var reader = cmd.ExecuteReader(); // Переменная для считывания данных
+                if (reader.HasRows) // Если reader имеет поля              
+                    while (reader.Read()) // Читает значения и переходит на следующие  
+                        result += reader.GetString(0); // Добавляет по строчке                
+            }
+            return result;
+        }
+        static public int Count_Rows_DB()
+        {
+            string result = string.Empty;
+            using (SQLiteConnection conn = new SQLiteConnection(path)) // Ресурс для подключения к БД
+            {
+                SQLiteCommand cmd = conn.CreateCommand();  // Создаём в ресурсе команду на выполнение запроса
+                cmd.CommandText = $"SELECT COUNT(name) FROM Names;"; // Формируем конкретную команду
+                conn.Open(); // Открываем соединение
+                var reader = cmd.ExecuteReader(); // Переменная для считывания данных
+                if (reader.HasRows) // Если reader имеет поля              
+                    while (reader.Read()) // Читает значения и переходит на следующие  
+                        result = reader.GetValue(0).ToString(); // Добавляет по строчке                
+            }
+            return Int32.Parse(result);
+        }
+        // Общий метод для подключения к БД и выполнения запросов
+        static private void MakeQuery(string query) 
+        {
+            SQLiteConnection conn = new SQLiteConnection(path); // Подключаемся к БД
+            SQLiteCommand cmd = conn.CreateCommand(); // Создаём в подключении команду на выполнение запроса
+            cmd.CommandText = query; // Формируем конкретную команду
+            conn.Open(); // Открываем соединение
+            cmd.ExecuteNonQuery(); // Выполянем команду
+            conn.Close(); // Закрываем соединение
+        }        
+    }
+}
---
+++ ComandsArgDemo/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComandsArgDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (!DBWork.IfDBExists()) // Проверка существования БД
+                DBWork.MakeBD(); // Создаём БД
+            else // Если БД существует, то пользователь выбирает, что с ней делать
+            {                
+                Console.Write("База данных существует.\nНажмите '1' для перезаписи существующей базы данных\n" +
+                    "Нажмите любую другую клавишу для дополнения базы данных.\nВаш выбор -> ");             
+                char symbol = Convert.ToChar(Console.ReadLine());
+                if (symbol == '1') // Если выбрана опция для перезаписи
+                {
+                    DBWork.DropTable();
+                    DBWork.MakeBD();
+                }
+            }
+            foreach (string el in args) // Цикл перебора аргументов командной строки
+            {                
+                Console.Write("Введите имя хобита -> "); // Ввод имён
+                DBWork.InitDB(el, Console.ReadLine()); // Заполнение таблицы БД 
+            }
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("\nИтоговая база данных:");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Аргумент cmd\tИмя\tДата и время");
+            Console.ForegroundColor = ConsoleColor.White;
+            int cnt = DBWork.Count_Rows_DB(); // Переменная для хранения кол-ва строк БД (отдельная переменная,
+                                              // чтобы в каждой итерации цикла не пересчитывать COUNT)
+            for (int i = 1; i <= cnt; ++i) // Начинаем с id = 1
+                Console.WriteLine(DBWork.GetData(i, "number_arg") + "\t\t" + DBWork.GetData(i, "name") + "\t" + DBWork.GetData(i, "date_time"));
+        }
+    }
+}
